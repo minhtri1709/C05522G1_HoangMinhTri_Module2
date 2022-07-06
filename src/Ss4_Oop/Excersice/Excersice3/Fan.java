698fae20b7ad26e5ad8f87251ba520790b408b3f
minhtri1709 <trihoangminh52@gmail.com>
2022-07-06 19:35:58 +0700

    add

---
+++ src/Ss4_Oop/Excersice/Excersice3/Fan.java
@@ -0,0 +1,73 @@
+package Ss4_Oop.Excersice.Excersice3;
+
+public class Fan {
+    boolean on;
+    String color;
+    int number, radius;
+    int speed;
+    static final int SLOW = 1;
+    static final int MEDIUM = 2;
+    static final int FAST = 3;
+
+
+    public Fan() {
+    }
+
+    private Fan(int number, int radius, String color, boolean on) {
+        this.number = number;
+        this.radius = radius;
+        this.color = color;
+        this.on = on;
+    }
+    public int getSLOW() {
+        return SLOW;
+    }
+
+    public int getMEDIUM() {
+        return MEDIUM;
+    }
+
+    public int getFAST() {
+        return FAST;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public String toString() {
+        if (isOn()) {
+            return "speed: " + getSpeed() + ", color: " + getColor() + ", radius: " + getRadius() + ", fan is on!";
+        }
+
+        return "color: " + getColor() + ", radius: " + getRadius() + ", fan is off!";
+    }
+}
---
+++ src/Ss4_Oop/Excersice/Excersice3/Main.java
@@ -0,0 +1,22 @@
+package Ss4_Oop.Excersice.Excersice3;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Fan fanFirst = new Fan();
+        fanFirst.setSpeed(fanFirst.getFAST());
+        fanFirst.setColor("Yellow");
+        fanFirst.setRadius(10);
+        fanFirst.setOn(true);
+
+        System.out.println("Your first fan \n" + fanFirst.toString());
+
+        Fan fanSecond = new Fan();
+        fanSecond.setSpeed(fanSecond.getMEDIUM());
+        fanSecond.setColor("Blue");
+        fanSecond.setRadius(5);
+        fanSecond.setOn(false);
+
+        System.out.println("Your second fan \n" + fanSecond.toString());
+    }
+}
